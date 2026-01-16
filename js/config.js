130d67f8ad1ae51323e0a01e24c4772e5340354c
takahiyo <141232864+takahiyo@users.noreply.github.com>
2026-01-16 09:07:46 +0900

    FireBaseDatabese再作成

--- js/config.js
+++ js/config.js
@@ -13,13 +13,13 @@ const PUBLIC_OFFICE_FALLBACKS = [];
 
 // Firebase Configuration (Compat版)
 const firebaseConfig = {
-    apiKey: "AIzaSyDRTr7h0diRJW6U1dQJaJgr303A5wm3aTE",
-    authDomain: "whereabouts-438df.firebaseapp.com",
-    projectId: "whereabouts-438df",
-    storageBucket: "whereabouts-438df.firebasestorage.app",
-    messagingSenderId: "955108979418",
-    appId: "1:955108979418:web:31a7235eeec873018dabe3",
-    measurementId: "G-26G0TS4HDW"
+  apiKey: "AIzaSyA_CKaAyt7aiZ0tXgv-0lHviCVV4y8urBQ",
+  authDomain: "whereabouts-f3388.firebaseapp.com",
+  projectId: "whereabouts-f3388",
+  storageBucket: "whereabouts-f3388.firebasestorage.app",
+  messagingSenderId: "578171146712",
+  appId: "1:578171146712:web:b36ba48f99eae97f6ba2ad",
+  measurementId: "G-SLXCBCX483"
 };
 
 // Initialize Firebase (Compat版)
